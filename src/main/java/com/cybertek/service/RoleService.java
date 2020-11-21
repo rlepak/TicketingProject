f5b8aa077c04ac55bb2a9e3db9833f1f3dc43139
cybertekschool <github@cybertekschool.com>
2020-11-21 14:08:22 -0500

    Ticketing Project MVC

---
+++ src/main/java/com/cybertek/service/RoleService.java
@@ -0,0 +1,10 @@
+package com.cybertek.service;
+
+import com.cybertek.dto.RoleDTO;
+import com.cybertek.dto.UserDTO;
+
+import java.util.List;
+
+public interface RoleService extends CrudService<RoleDTO,Long> {
+
+}
