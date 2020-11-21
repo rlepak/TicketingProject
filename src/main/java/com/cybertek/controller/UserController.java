8e5cca4b9ae2e635f6330438483fa23f81a6c190
cybertekschool <github@cybertekschool.com>
2020-11-21 15:20:49 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+
+
     @GetMapping("/create")
     public String createUser(Model model){
 
