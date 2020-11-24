6bc5cad695f2db10be8c75c842b7d4686c3c121c
cybertekschool <github@cybertekschool.com>
2020-11-24 18:59:25 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -2,6 +2,9 @@ package com.cybertek.controller;
 
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.dto.UserDTO;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
-
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
 
         return "/user/create";
     }
