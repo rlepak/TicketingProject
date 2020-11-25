6dfaf3587227f4388a1a6537c4b191768802397c
cybertekschool <github@cybertekschool.com>
2020-11-24 19:17:37 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,4 +29,35 @@ public class UserController {
 
         return "/user/create";
     }
+
+    @PostMapping("create")
+    public String insertUser(UserDTO user,Model model){
+
+        userService.save(user);
+
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/create";
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
