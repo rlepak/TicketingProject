f0b5de175bf01dfab01bd9789d45e5ec66adda90
cybertekschool <github@cybertekschool.com>
2020-11-24 21:21:50 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,7 +31,7 @@ public class UserController {
         return "/user/create";
     }
 
-    @PostMapping("create")
+    @PostMapping("/create")
     public String insertUser(UserDTO user,Model model){
 
         userService.save(user);
@@ -41,8 +42,30 @@ public class UserController {
 
         return "/user/create";
 
+    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username,Model model){
+
+        model.addAttribute("user",userService.findById(username));
+        model.addAttribute("users",userService.findAll());
+        model.addAttribute("roles",roleService.findAll());
+
+        return "/user/update";
 
     }
+
+    @PostMapping("/update/{username}")
+    public String updateUser(@PathVariable("username") String username,Model model){
+
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/create";
+    }
+
+
 
 
 
