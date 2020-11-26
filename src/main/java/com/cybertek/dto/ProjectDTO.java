71a1be11df67313d9a2f1a3f71d944dfb17dffdf
cybertekschool <github@cybertekschool.com>
2020-11-25 19:54:08 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/dto/ProjectDTO.java
+++ src/main/java/com/cybertek/dto/ProjectDTO.java
@@ -1,9 +1,17 @@
 package com.cybertek.dto;
 
 import com.cybertek.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ProjectDTO {
 
     private String projectName;
