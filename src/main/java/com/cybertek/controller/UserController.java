a80b13f911ce1fb0e8b35b8bed9b2e06a748a370
cybertekschool <github@cybertekschool.com>
2020-11-25 19:12:37 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -54,6 +54,12 @@ public class UserController {
         return "redirect:/user/create";
     }
 
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+        userService.deleteById(username);
+        return "redirect:/user/create";
+    }
+
 
 
 
