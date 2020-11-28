f8666ffaf8f7d1ec5c68c719d4a7d82df1ff11ed
cybertekschool <github@cybertekschool.com>
2020-11-28 14:46:09 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/task")
@@ -42,6 +43,10 @@ public class TaskController {
 
         task.setTaskStatus(Status.OPEN);
         task.setAssignedDate(LocalDate.now());
+        task.setId(UUID.randomUUID().getMostSignificantBits());
+
+        System.out.println("Auto generated ID : " + task.getId());
+
         taskService.save(task);
 
         return "redirect:/task/create";
